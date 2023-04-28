934b6df7bd32db89d21981c727d44809141af6c3
largueza1 <dsl.runescape@gmail.com>
2023-04-27 22:17:33 -0300

    javed

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,5 +1,9 @@
 const chk = document.getElementById('chk')
+const navcc = document.getElementById('navCC')
+
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
+    navcc.classList.remove('bg-light')
+    navcc.style.color("#FFFF")
 })
